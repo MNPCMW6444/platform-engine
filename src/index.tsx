9a9edd9e13dc2d7af18be22963dad2a223720727
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-12-10 14:28:22 +0200

    good

--- src/index.tsx
+++ src/index.tsx
@@ -4,62 +4,15 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { from, Observable } from "rxjs";
-import { epoch, fft, powerByBand } from "@neurosity/pipes";
-import { FrequencyBands } from "./constants";
-import { FrequencyRangeInHz } from "./types";
+//import { from, Observable } from "rxjs";
+//import { epoch, fft, powerByBand } from "@neurosity/pipes";
+//import { FrequencyBands } from "./constants";
+//import { FrequencyRangeInHz } from "./types";
 
 let x;
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
-
-const state$ = from(store);
-
-type StoreType = typeof store;
-
-function getState$(store: StoreType) {
-  return new Observable(function (observer) {
-    // emit the current state as first value:
-    observer.next(
-      store.getState().muse.museReadings[
-        store.getState().muse.museReadings.length - 1
-      ]
-    );
-    const unsubscribe = store.subscribe(function () {
-      // emit on every new state changes
-      observer.next(
-        store.getState().muse.museReadings[
-          store.getState().muse.museReadings.length - 1
-        ]
-      );
-    });
-    // let's return the function that will be called
-    // when the Observable is unsubscribed
-    return unsubscribe;
-  });
-}
-const freqnames = Object.keys(FrequencyBands);
-const freqrange: FrequencyRangeInHz[] = Object.values(FrequencyBands);
-
-const frequencyBands = {} as any;
-
-freqnames.forEach((freqname: string, index: number) => {
-  frequencyBands[freqname] = [
-    freqrange[index].minFrequencyiInHz,
-    freqrange[index].maxFrequencyiInHz,
-  ];
-});
-
-getState$(store)
-  .pipe(
-    epoch({ duration: 256, interval: 100 }),
-    fft({ bins: 256 }),
-    powerByBand(frequencyBands)
-  )
-  .subscribe((y) => {
-    x = y;
-  });
 
 root.render(
   <React.StrictMode>
--- src/ui/Testing.tsx
+++ src/ui/Testing.tsx
@@ -4,15 +4,72 @@ import { orange } from "@mui/material/colors";
 import Grid from "@mui/material/Grid";
 import { useState } from "react";
 
-import mexp from "math-expression-evaluator";
+//import mexp from "math-expression-evaluator";
+import { evaluate } from "mathjs";
+import { from, Observable } from "rxjs";
+
+import { store } from "../store/store";
+import { epoch, fft, powerByBand } from "@neurosity/pipes";
+import { FrequencyBands } from "../constants";
+import { FrequencyRangeInHz } from "../types";
 
 interface TestingProps {
   x: any;
 }
 
+const state$ = from(store);
+
+type StoreType = typeof store;
+
+function getState$(store: StoreType) {
+  return new Observable(function (observer) {
+    // emit the current state as first value:
+    observer.next(
+      store.getState().muse.museReadings[
+        store.getState().muse.museReadings.length - 1
+      ]
+    );
+    const unsubscribe = store.subscribe(function () {
+      // emit on every new state changes
+      observer.next(
+        store.getState().muse.museReadings[
+          store.getState().muse.museReadings.length - 1
+        ]
+      );
+    });
+    // let's return the function that will be called
+    // when the Observable is unsubscribed
+    return unsubscribe;
+  });
+}
+
+const freqnames = Object.keys(FrequencyBands);
+const freqrange: FrequencyRangeInHz[] = Object.values(FrequencyBands);
+
+const frequencyBands = {} as any;
+
+freqnames.forEach((freqname: string, index: number) => {
+  frequencyBands[freqname] = [
+    freqrange[index].minFrequencyiInHz,
+    freqrange[index].maxFrequencyiInHz,
+  ];
+});
+
 const Testing = ({ x }: TestingProps) => {
-  const [it, setIt] = useState<string>("");
+  // const [it, setIt] = useState<string>("");
   const [r, setr] = useState<string>("");
+
+  const [s, sets] = useState<any>();
+
+  getState$(store)
+    .pipe(
+      epoch({ duration: 256, interval: 100 }),
+      fft({ bins: 256 }),
+      powerByBand(frequencyBands)
+    )
+    .subscribe((y) => {
+      sets(y);
+    });
 
   return (
     <Grid
@@ -29,22 +86,22 @@ const Testing = ({ x }: TestingProps) => {
             <Typography sx={{ fontSize: "0.8rem" }}>Theta</Typography>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[Theta-A1]")}>
+            <Button onClick={() => setr((prev) => prev + " ThetaA1 ")}>
               A1
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[Theta-A2]")}>
+            <Button onClick={() => setr((prev) => prev + " ThetaA2 ")}>
               A2
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[Theta-F7]")}>
+            <Button onClick={() => setr((prev) => prev + " ThetaF7 ")}>
               F7
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[Theta-F8]")}>
+            <Button onClick={() => setr((prev) => prev + " ThetaF8 ")}>
               F8
             </Button>
           </Grid>
@@ -55,22 +112,22 @@ const Testing = ({ x }: TestingProps) => {
             <Typography sx={{ fontSize: "0.8rem" }}>AlphaLow</Typography>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[AlphaLow-A1]")}>
+            <Button onClick={() => setr((prev) => prev + " AlphaLowA1 ")}>
               A1
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[AlphaLow-A2]")}>
+            <Button onClick={() => setr((prev) => prev + " AlphaLowA2 ")}>
               A2
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[AlphaLow-F7]")}>
+            <Button onClick={() => setr((prev) => prev + " AlphaLowF7 ")}>
               F7
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[AlphaLow-F8]")}>
+            <Button onClick={() => setr((prev) => prev + " AlphaLowF8 ")}>
               F8
             </Button>
           </Grid>
@@ -80,22 +137,22 @@ const Testing = ({ x }: TestingProps) => {
             <Typography sx={{ fontSize: "0.8rem" }}>AlphaHigh</Typography>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[AlphaHigh-A1]")}>
+            <Button onClick={() => setr((prev) => prev + " AlphaHighA1 ")}>
               A1
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[AlphaHigh-A2]")}>
+            <Button onClick={() => setr((prev) => prev + " AlphaHighA2 ")}>
               A2
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[AlphaHigh-F7]")}>
+            <Button onClick={() => setr((prev) => prev + " AlphaHighF7 ")}>
               F7
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[AlphaHigh-F8]")}>
+            <Button onClick={() => setr((prev) => prev + " AlphaHighF8 ")}>
               F8
             </Button>
           </Grid>
@@ -105,22 +162,22 @@ const Testing = ({ x }: TestingProps) => {
             <Typography sx={{ fontSize: "0.8rem" }}>BetaLow</Typography>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaLow-A1]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaLowA1 ")}>
               A1
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaLow-A2]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaLowA2 ")}>
               A2
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaLow-F7]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaLowF7 ")}>
               F7
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaLow-F8]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaLowF8 ")}>
               F8
             </Button>
           </Grid>
@@ -130,22 +187,22 @@ const Testing = ({ x }: TestingProps) => {
             <Typography sx={{ fontSize: "0.8rem" }}>BetaMid</Typography>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaMid-A1]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaMidA1 ")}>
               A1
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaMid-A2]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaMidA2 ")}>
               A2
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaMid-F7]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaMidF7 ")}>
               F7
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaMid-F8]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaMidF8 ")}>
               F8
             </Button>
           </Grid>
@@ -155,22 +212,22 @@ const Testing = ({ x }: TestingProps) => {
             <Typography sx={{ fontSize: "0.8rem" }}>BetaHigh</Typography>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaHigh-A1]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaHighA1 ")}>
               A1
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaHigh-A2]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaHighA2 ")}>
               A2
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaHigh-F7]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaHighF7 ")}>
               F7
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[BetaHigh-F8]")}>
+            <Button onClick={() => setr((prev) => prev + " BetaHighF8 ")}>
               F8
             </Button>
           </Grid>
@@ -180,22 +237,22 @@ const Testing = ({ x }: TestingProps) => {
             <Typography sx={{ fontSize: "0.8rem" }}>Gamma</Typography>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[Gamma-A1]")}>
+            <Button onClick={() => setr((prev) => prev + " GammaA1 ")}>
               A1
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[Gamma-A2]")}>
+            <Button onClick={() => setr((prev) => prev + " GammaA2 ")}>
               A2
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[Gamma-F7]")}>
+            <Button onClick={() => setr((prev) => prev + " GammaF7 ")}>
               F7
             </Button>
           </Grid>
           <Grid item>
-            <Button onClick={() => setr((prev) => prev + "[Gamma-F8]")}>
+            <Button onClick={() => setr((prev) => prev + " GammaF8 ")}>
               F8
             </Button>
           </Grid>
@@ -219,19 +276,56 @@ const Testing = ({ x }: TestingProps) => {
           onChange={(e) => setr(e.target.value)}
         />
       </Grid>
-      <Grid item>{parseNFTModel(it)}</Grid>
+      <Grid item>Muse {!s && "dis"}Connected</Grid>
+      <Grid item>{parseNFTModel(r, s)}</Grid>
     </Grid>
   );
 };
 
-const parseNFTModel = (nfTString: string) => {
+const parseNFTModel = (nfTString: string, x: any) => {
+  try {
+    let exp = nfTString;
+
+    debugger;
+    const res = evaluate(exp, {
+      ThetaA1: x.THETA[0],
+      ThetaA2: x.THETA[1],
+      ThetaF7: x.THETA[2],
+      ThetaF8: x.THETA[3],
+      AlphaLowA1: x.ALPHA_LOW[0],
+      AlphaLowA2: x.ALPHA_LOW[1],
+      AlphaLowF7: x.ALPHA_LOW[2],
+      AlphaLowF8: x.ALPHA_LOW[3],
+      AlphaHighA1: x.ALPHA_HIGH[0],
+      AlphaHighA2: x.ALPHA_HIGH[1],
+      AlphaHighF7: x.ALPHA_HIGH[2],
+      AlphaHighF8: x.ALPHA_HIGH[3],
+      BetaLowA1: x.BETA_LOW[0],
+      BetaLowA2: x.BETA_LOW[1],
+      BetaLowF7: x.BETA_LOW[2],
+      BetaLowF8: x.BETA_LOW[3],
+      BetaMidA1: x.BETA_MID[0],
+      BetaMidA2: x.BETA_MID[1],
+      BetaMidF7: x.BETA_MID[2],
+      BetaMidF8: x.BETA_MID[3],
+      BetaHighA1: x.BETA_HIGH[0],
+      BetaHighA2: x.BETA_HIGH[1],
+      BetaHighF7: x.BETA_HIGH[2],
+      BetaHighF8: x.BETA_HIGH[3],
+      GammaA1: x.GAMMA[0],
+      GammaA2: x.GAMMA[1],
+      GammaF7: x.GAMMA[2],
+      GammaF8: x.GAMMA[3],
+    });
+    return res;
+  } catch (e) {}
   try {
     let parStart = -1;
     parStart = nfTString.indexOf("(");
     let parEnd = -1;
     parEnd = nfTString.indexOf(")");
     let inside = nfTString.substring(parStart, parEnd);
-    let outside = nfTString.replace(inside, "");
+    let outside = nfTString;
     let isParInStart = parStart === 0;
     let isParInEnd = parEnd === nfTString.length - 1;
   } catch (e) {
